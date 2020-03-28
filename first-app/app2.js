ef296ea13cc0c0a8129bb40e2ab6a2203fc60b22
Magda Cembor <magda.cembor@gmail.com>
2020-03-28 10:50:25 +0000

    modules

---
+++ first-app/app2.js
@@ -0,0 +1,27 @@
+const message = '';
+console.log(global.message);
+
+console.log(module);
+// every file is a module, all module variables are available ony inside this particular module
+
+
+
+
+
+// console.log(); // global objects:
+
+// setTimeout()
+// clearTimeout();
+
+// setInterval()
+// clearInterval()
+
+
+// browsers:
+// window.console.log 
+// or
+// console.log
+// in node it's 'global'
+// globalThis.setTimeout
+
+
